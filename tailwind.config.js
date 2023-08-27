a6aa4a483ecd6b3552cf488b89efa9fbecb96ebc
Glory-mmachi <105730131+Glory-mmachi@users.noreply.github.com>
2023-08-27 14:22:59 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'lightGrey':'#F8F9FA',
+        'footerGrey':'#F2F2F2'
+      },
+     
+
+    },
+  },
+  plugins: [],
+}
+
